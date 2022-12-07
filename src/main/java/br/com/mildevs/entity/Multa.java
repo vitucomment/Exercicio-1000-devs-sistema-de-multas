4aeaa8e1adcd1db90f2cd3b2a54bac95bc0e35e7
vitucomment <victorpy.1999@gmail.com>
2022-12-07 13:10:31 -0300

    adicionando TXT sobre Fetch-Cascade-Optional

--- src/main/java/br/com/mildevs/entity/Multa.java
+++ src/main/java/br/com/mildevs/entity/Multa.java
@@ -3,6 +3,7 @@ package br.com.mildevs.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
